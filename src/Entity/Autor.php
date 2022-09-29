6f6eaa7ce5424ea9d5f668563f88a50fe59e7cbc
jairoparedes <hiro.paredes.candia@gmail.com>
2022-09-29 19:36:05 -0300

    Se completan las entidades

--- src/Entity/Autor.php
+++ src/Entity/Autor.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\AutorRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AutorRepository::class)]
@@ -13,8 +14,83 @@ class Autor
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 1)]
+    private ?string $sexo = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $fecha_nacimiento = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nacionalidad = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $apellido = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getSexo(): ?string
+    {
+        return $this->sexo;
+    }
+
+    public function setSexo(string $sexo): self
+    {
+        $this->sexo = $sexo;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->fecha_nacimiento;
+    }
+
+    public function setFechaNacimiento(\DateTimeInterface $fecha_nacimiento): self
+    {
+        $this->fecha_nacimiento = $fecha_nacimiento;
+
+        return $this;
+    }
+
+    public function getNacionalidad(): ?string
+    {
+        return $this->nacionalidad;
+    }
+
+    public function setNacionalidad(string $nacionalidad): self
+    {
+        $this->nacionalidad = $nacionalidad;
+
+        return $this;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): self
+    {
+        $this->apellido = $apellido;
+
+        return $this;
     }
 }
--- src/Entity/Libro.php
+++ src/Entity/Libro.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\LibroRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: LibroRepository::class)]
@@ -13,8 +14,83 @@ class Libro
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titulo = null;
+
+    #[ORM\Column]
+    private ?int $numero_paginas = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $descripcion = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $portada = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getTitulo(): ?string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getNumeroPaginas(): ?int
+    {
+        return $this->numero_paginas;
+    }
+
+    public function setNumeroPaginas(int $numero_paginas): self
+    {
+        $this->numero_paginas = $numero_paginas;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getPortada(): ?string
+    {
+        return $this->portada;
+    }
+
+    public function setPortada(string $portada): self
+    {
+        $this->portada = $portada;
+
+        return $this;
     }
 }
