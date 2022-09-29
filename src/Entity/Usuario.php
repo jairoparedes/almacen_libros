68cea59181db12b20f6c014b0429800ecc80cf4f
jairoparedes <hiro.paredes.candia@gmail.com>
2022-09-29 17:48:28 -0300

    Creacion base de entidades

---
+++ src/Entity/Usuario.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsuarioRepository::class)]
+class Usuario
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
